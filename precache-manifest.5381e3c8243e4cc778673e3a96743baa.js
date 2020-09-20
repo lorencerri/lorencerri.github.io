7a5d45829f803bc3db57c04f660de75541b1a2c2
Loren Cerri <truexpixels@gmail.com>
2020-09-19 18:06:37 -0700

    Updates

---
+++ precache-manifest.5381e3c8243e4cc778673e3a96743baa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377ab4e8ab2cd0de517bacff05b6c4b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada197590292d201c6ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2ae2947e4d0bdb10f53",
+    "url": "/static/js/2.faa5b2f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.faa5b2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada197590292d201c6ef",
+    "url": "/static/js/main.d9461647.chunk.js"
+  },
+  {
+    "revision": "d94858d2f1cb5de89c85",
+    "url": "/static/js/runtime-main.dc4b3e3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
