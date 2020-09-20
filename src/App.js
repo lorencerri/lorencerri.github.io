e575bc43815db5175753c4689f011a979e5c6f16
Loren Cerri <truexpixels@gmail.com>
2020-09-19 19:42:13 -0700

    add repo cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
+    const Card = (repo) => {
+        return (
+            <div
+                style={{ margin: 10 }}
+                class="repo-card"
+                data-repo={repo}
+            ></div>
+        );
+    };
+
     return (
         <div className="App">
             <header className="App-header">
@@ -21,6 +31,13 @@ function App() {
                         <a href="https://twitter.com/truexpixels">Twitter</a>
                     </font>
                 </div>
+                <font size="4" style={{ margin: 10 }}>
+                    Notable Projects
+                </font>
+                {Card("plexidev/quick.db")}
+                {Card("TrueXPixels/discord-guardian")}
+                {Card("TrueXPixels/powercord-reverse-image-search")}
+                {Card("TrueXPixels/quick.hook")}
             </header>
         </div>
     );
