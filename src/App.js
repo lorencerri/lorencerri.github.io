d866879c6c1cdb87f9e0e1010f1c6679e22f982f
Loren Cerri <truexpixels@gmail.com>
2020-09-19 19:29:12 -0700

    Add component library

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div style={{ margin: 50 }}>
+                    <h3
+                        style={{
+                            marginBottom: 0,
+                            borderBottom: "1px solid grey",
+                        }}
+                    >
+                        TrueXPixels
+                    </h3>
+                    <font size="4">
+                        <a href="https://github.com/truexpixels">GitHub</a>{" "}
+                        &bull;{" "}
+                        <a href="https://twitter.com/truexpixels">Twitter</a>
+                    </font>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
