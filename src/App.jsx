86cc2698ec506567dc42af2e6be8f363fba95349
TrueXPixels <truexpixels@gmail.com>
2020-10-06 10:56:17 -0700

    Add .prettierrc.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,154 +1,154 @@
-import React, { useState, useEffect } from "react";
-import { Card } from "./Card.jsx";
-import projects from "./projects.json";
-import styled from "styled-components";
-import { useHistory } from "react-router-dom";
-import FadeIn from "react-fade-in";
-import { Tooltip } from "antd";
+import React, {useState, useEffect} from 'react';
+import {Card} from './Card.jsx';
+import projects from './projects.json';
+import styled from 'styled-components';
+import {useHistory} from 'react-router-dom';
+import FadeIn from 'react-fade-in';
+import {Tooltip} from 'antd';
 
-import NPMSVG from "./icons/npm.svg";
-import DiscordSVG from "./icons/discord-logo.svg";
-import PowercordSVG from "./icons/powercord.svg";
+import NPMSVG from './icons/npm.svg';
+import DiscordSVG from './icons/discord-logo.svg';
+import PowercordSVG from './icons/powercord.svg';
 
 const tags = [
-    {
-        icon: NPMSVG,
-        name: "npm",
-        tooltip: "npm",
-    },
-    {
-        icon: DiscordSVG,
-        name: "discord",
-        tooltip: "Discord",
-    },
-    {
-        icon: PowercordSVG,
-        name: "powercord",
-        tooltip: "Powercord",
-    },
+	{
+		icon: NPMSVG,
+		name: 'npm',
+		tooltip: 'npm'
+	},
+	{
+		icon: DiscordSVG,
+		name: 'discord',
+		tooltip: 'Discord'
+	},
+	{
+		icon: PowercordSVG,
+		name: 'powercord',
+		tooltip: 'Powercord'
+	}
 ];
 
 export const App = () => {
-    const history = useHistory();
-    const [filter, setFilter] = useState(
-        new URLSearchParams(history.location.search).get("tag")
-    );
+	const history = useHistory();
+	const [filter, setFilter] = useState(
+		new URLSearchParams(history.location.search).get('tag')
+	);
 
-    useEffect(() => {
-        history.push(filter ? `/?tag=${filter}` : "");
-    }, [history, filter]);
+	useEffect(() => {
+		history.push(filter ? `/?tag=${filter}` : '');
+	}, [history, filter]);
 
-    return (
-        <AppContainer>
-            <AppHeader key={filter}>
-                <AppHeaderContainer>
-                    <AppHeaderTitle>Loren Cerri</AppHeaderTitle>
-                    <font size="4">
-                        <a
-                            href="https://github.com/lorencerri"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            GitHub
-                        </a>
-                        &nbsp;&bull;&nbsp;
-                        <a
-                            href="https://twitter.com/lorencerri"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Twitter
-                        </a>
-                    </font>
-                </AppHeaderContainer>
+	return (
+		<AppContainer>
+			<AppHeader key={filter}>
+				<AppHeaderContainer>
+					<AppHeaderTitle>Loren Cerri</AppHeaderTitle>
+					<font size='4'>
+						<a
+							href='https://github.com/lorencerri'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							GitHub
+						</a>
+						&nbsp;&bull;&nbsp;
+						<a
+							href='https://twitter.com/lorencerri'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							Twitter
+						</a>
+					</font>
+				</AppHeaderContainer>
 
-                {/* Tags */}
-                <TagsContainer>
-                    <VerticalSpan>Filter: &nbsp;</VerticalSpan>
-                    {tags.map((tag) => (
-                        <Tooltip title={tag.tooltip}>
-                            <SVG
-                                src={tag.icon}
-                                alt={tag.name}
-                                key={tag.name}
-                                toggled={filter === tag.name}
-                                onClick={() => {
-                                    if (filter === tag.name) setFilter(null);
-                                    else setFilter(tag.name);
-                                }}
-                            ></SVG>
-                        </Tooltip>
-                    ))}
-                </TagsContainer>
+				{/* Tags */}
+				<TagsContainer>
+					<VerticalSpan>Filter: &nbsp;</VerticalSpan>
+					{tags.map(tag => (
+						<Tooltip title={tag.tooltip}>
+							<SVG
+								src={tag.icon}
+								alt={tag.name}
+								key={tag.name}
+								toggled={filter === tag.name}
+								onClick={() => {
+									if (filter === tag.name) setFilter(null);
+									else setFilter(tag.name);
+								}}
+							></SVG>
+						</Tooltip>
+					))}
+				</TagsContainer>
 
-                {/* Projects */}
-                <FadeIn key={filter} delay={150}>
-                    {projects
-                        .filter((p) => !filter || p.tag === filter)
-                        .map((data, index) => (
-                            <Card key={`project-${index}`} {...data}></Card>
-                        ))}
-                </FadeIn>
-            </AppHeader>
-        </AppContainer>
-    );
+				{/* Projects */}
+				<FadeIn key={filter} delay={150}>
+					{projects
+						.filter(p => !filter || p.tag === filter)
+						.map((data, index) => (
+							<Card key={`project-${index}`} {...data}></Card>
+						))}
+				</FadeIn>
+			</AppHeader>
+		</AppContainer>
+	);
 };
 
 const AppHeaderTitle = styled.div`
-    margin-bottom: 0;
-    border-bottom: 1px solid grey;
+	margin-bottom: 0;
+	border-bottom: 1px solid grey;
 `;
 
 const AppHeaderContainer = styled.div`
-    margin: 50px;
+	margin: 50px;
 `;
 
 const VerticalSpan = styled.span`
-    vertical-align: middle;
+	vertical-align: middle;
 `;
 
 const AppContainer = styled.div`
-    text-align: center;
+	text-align: center;
 `;
 
 const AppHeader = styled(FadeIn)`
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-    margin-bottom: 50px;
-    overflow: auto;
-    -ms-overflow-style: none;
-    scrollbar-color: transparent transparent;
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+	margin-bottom: 50px;
+	overflow: auto;
+	-ms-overflow-style: none;
+	scrollbar-color: transparent transparent;
 `;
 
 const TagsContainer = styled.div`
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
-        sans-serif, Apple Color Emoji, Segoe UI Emoji;
-    border: 1px solid #373c3e;
-    border-radius: 6px;
-    background: #181a1b;
-    padding: 10px;
-    font-size: 14px;
-    line-height: 1.5;
-    color: #aba499;
-    margin: 5px;
-    min-width: 33vw;
-    max-width: 473px;
+	font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
+		sans-serif, Apple Color Emoji, Segoe UI Emoji;
+	border: 1px solid #373c3e;
+	border-radius: 6px;
+	background: #181a1b;
+	padding: 10px;
+	font-size: 14px;
+	line-height: 1.5;
+	color: #aba499;
+	margin: 5px;
+	min-width: 33vw;
+	max-width: 473px;
 `;
 
 const SVG = styled.img`
-    filter: brightness(0) saturate(100%)
-        ${({ toggled }) =>
-            toggled
-                ? "invert(100%) sepia(0%) saturate(7500%) hue-rotate(302deg) brightness(117%) contrast(102%)"
-                : "invert(20%) sepia(3%) saturate(1583%) hue-rotate(152deg) brightness(91%) contrast(85%)"};
-    width: 24px;
-    vertical-align: middle;
-    margin: 0 5px;
-    cursor: pointer;
+	filter: brightness(0) saturate(100%)
+		${({toggled}) =>
+			toggled
+				? 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(302deg) brightness(117%) contrast(102%)'
+				: 'invert(20%) sepia(3%) saturate(1583%) hue-rotate(152deg) brightness(91%) contrast(85%)'};
+	width: 24px;
+	vertical-align: middle;
+	margin: 0 5px;
+	cursor: pointer;
 `;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { App } from "./App";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import {App} from './App';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-import "antd/dist/antd.dark.css";
-import "./index.css";
+import 'antd/dist/antd.dark.css';
+import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
 );
