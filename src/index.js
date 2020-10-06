50075c8401cbcf90cd200d3e709b48bf91ffbb9b
TrueXPixels <truexpixels@gmail.com>
2020-10-06 13:14:08 -0700

    Convert .css file to styled-components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { GlobalStyle } from './indexStyles';
 
 import 'antd/dist/antd.dark.css';
-import './index.css';
 
 ReactDOM.render(
 	<BrowserRouter>
+		<GlobalStyle />
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
