ca4373d2a7ab620ee71cd50a980b733fd0a478f7
Loren Cerri <lorencerri@gmail.com>
2020-10-16 21:04:08 -0700

    Add dynamic stats

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { get } from 'lodash';
 import { Tooltip } from 'antd';
 import PropTypes from 'prop-types';
 import {
@@ -24,44 +25,75 @@ const Card = ({
     icon = '',
     language = '',
     stats = [],
-}) => (
-    <CardContainer>
-        <CardTitleContainer>
-            <SVG
-                header
-                src={require(`../Assets/Icons/${icon}.svg`)}
-                alt={title}
-            />
-            <CardTitle>
-                <CardTitleAnchor
-                    href={link}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    {title}
-                </CardTitleAnchor>
-            </CardTitle>
-        </CardTitleContainer>
-        <CardDescription>{description}</CardDescription>
-        <CardStatsContainer>
-            <CardLanguageContainer language={language}>
-                <CardLanguageSymbol color={language.color} />
-                <CenterSpan>{language.text}</CenterSpan>
-            </CardLanguageContainer>
-            {stats.map((i, index) => (
-                <Tooltip title={i.alt}>
-                    <StatsContainer key={`${title}-stat-${index}`}>
-                        <SVG
-                            src={require(`../Assets/Icons/${i.icon}.svg`)}
-                            alt={i.alt}
-                        />
-                        <CenterSpan>{i.text}</CenterSpan>
-                    </StatsContainer>
-                </Tooltip>
-            ))}
-        </CardStatsContainer>
-    </CardContainer>
-);
+}) => {
+    const [state, setState] = useState({});
+
+    useEffect(() => {
+        const fetchStats = async () => {
+            const needToFetch = stats.filter(
+                item => typeof item.text === 'object'
+            );
+
+            for (let i = 0; i < needToFetch.length; i += 1) {
+                const item = needToFetch[i].text;
+
+                const res = await fetch(item.url);
+                const json = await res.json();
+
+                setState({
+                    [item.url + item.path]: get(json, item.path),
+                });
+            }
+        };
+
+        fetchStats();
+    }, [stats]);
+
+    const cardStats = stats.map((i, index) => (
+        <Tooltip title={i.alt}>
+            <StatsContainer key={`${title}-stat-${index}`}>
+                <SVG
+                    src={require(`../Assets/Icons/${i.icon}.svg`)}
+                    alt={i.alt}
+                />
+                <CenterSpan>
+                    {typeof i.text === 'object'
+                        ? state[i.text.url + i.text.path] || 'Loading...'
+                        : i.text}
+                </CenterSpan>
+            </StatsContainer>
+        </Tooltip>
+    ));
+
+    return (
+        <CardContainer>
+            <CardTitleContainer>
+                <SVG
+                    header
+                    src={require(`../Assets/Icons/${icon}.svg`)}
+                    alt={title}
+                />
+                <CardTitle>
+                    <CardTitleAnchor
+                        href={link}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        {title}
+                    </CardTitleAnchor>
+                </CardTitle>
+            </CardTitleContainer>
+            <CardDescription>{description}</CardDescription>
+            <CardStatsContainer>
+                <CardLanguageContainer language={language}>
+                    <CardLanguageSymbol color={language.color} />
+                    <CenterSpan>{language.text}</CenterSpan>
+                </CardLanguageContainer>
+                {cardStats}
+            </CardStatsContainer>
+        </CardContainer>
+    );
+};
 
 Card.propTypes = {
     title: PropTypes.string,
